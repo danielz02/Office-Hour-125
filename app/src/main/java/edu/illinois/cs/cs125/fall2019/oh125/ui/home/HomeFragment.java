13a208ad7ef3159d3ebd5a6d8b522677b1567aae
danielz02 <zchdz0215@gmail.com>
2019-12-08 23:08:52 -0600

    Refactor code

--- app/src/main/java/edu/illinois/cs/cs125/fall2019/oh125/ui/home/HomeFragment.java
+++ app/src/main/java/edu/illinois/cs/cs125/fall2019/oh125/ui/home/HomeFragment.java
@@ -117,8 +117,6 @@ public class HomeFragment extends Fragment {
             // Set queue request and exit queue buttons invisible
             Button queue = getView().findViewById(R.id.queueRequestButton);
             queue.setVisibility(View.GONE);
-
-            addChangeListener(view);
         } else {
             // Student
             final Button queue = getView().findViewById(R.id.queueRequestButton);
@@ -153,7 +151,8 @@ public class HomeFragment extends Fragment {
                                             @Override
                                             public void onComplete(@NonNull Task<Void> task) {
                                                 if (task.isSuccessful()) {                                                        
-                                                    Log.i("Exited Queue", student.toString() + " exited");
+                                                    Log.i("Exited Queue",
+                                                            student.toString() + " exited");
 
                                                     exitQueue.setVisibility(View.GONE);
                                                     queue.setVisibility(View.VISIBLE);
@@ -185,8 +184,6 @@ public class HomeFragment extends Fragment {
                     }
                 }
             });
-
-            addChangeListener(view);
         }
 
         // Button "I'm at Office Hour" and "Leave Office Hour"
@@ -277,14 +274,6 @@ public class HomeFragment extends Fragment {
                 }
             }
         });
-
-    }
-
-    /**
-     * A method to register for all change listener of data summary
-     */
-    private void addChangeListener(final View view) {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         TextView studentCount = view.findViewById(R.id.studentCount);
         try {
